8f804837971bcfb5ef4d4140cf72392a9b071f95
Alex Sutton <alexsutton14@hotmail.com>
2019-10-25 22:38:13 +0100

    Game working

--- hangman.rb
+++ hangman.rb
@@ -30,20 +30,24 @@ get "/" do
     if params["reset"] == "true"
         session["word"] = new_word()
         session["unplayed_letters"] = generate_alphabet
+        session["played_letters"] = []
     end
     if !session["word"]
         session["word"] = new_word()
         session["unplayed_letters"] = generate_alphabet
+        session["played_letters"] = []
     end
     if !session["unplayed_letters"]
         session["unplayed_letters"] = generate_alphabet
+        session["played_letters"] = []
     end
     if params["choice"]
         choice_index = params["choice"].to_i
         session["unplayed_letters"] = remove_choice(session["unplayed_letters"], choice_index)
-        flash_message = "Removed: " + ALPHABET[choice_index]
+        session["played_letters"] << ALPHABET[choice_index]
     end
     word_letters = session["word"].split("")
     erb :index, :locals => {:word => session["word"], :unplayed_letters => session["unplayed_letters"], 
-        :alphabet => ALPHABET, :flash_message => flash_message, :word_letters => word_letters}
+        :alphabet => ALPHABET, :flash_message => flash_message,:word_letters => word_letters, 
+        :played_letters => session["played_letters"]}
 end
