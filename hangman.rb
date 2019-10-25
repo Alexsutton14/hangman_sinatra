3c991b311cba6117e67796911ebbb19ae1edf586
Alex Sutton <alexsutton14@hotmail.com>
2019-10-25 22:28:04 +0100

    Letter selection working, working on game letters

--- hangman.rb
+++ hangman.rb
@@ -3,15 +3,15 @@ require "sinatra/reloader" if development?
 
 configure do
     enable :sessions
-    #set :session_secret, "secret"
 end
 
 flash_message = ""
 
 ALPHABET = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
-FULL_ALPHABET = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-
+def generate_alphabet()
+    return ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+end
 
 WORDS = [
     "hangman"
@@ -27,24 +27,23 @@ def remove_choice(input_unplayed_array, choice_index)
 end
 
 get "/" do
-    @session = session
-    if params["reset"] = "true"
-        @session["word"] = new_word()
-        @session["unplayed_letters"] = ALPHABET
+    if params["reset"] == "true"
+        session["word"] = new_word()
+        session["unplayed_letters"] = generate_alphabet
     end
-    if !@session["word"]
-        @session["word"] = new_word()
-        @session["unplayed_letters"] = ALPHABET
+    if !session["word"]
+        session["word"] = new_word()
+        session["unplayed_letters"] = generate_alphabet
     end
-    if !@session["unplayed_letters"]
-        @session["unplayed_letters"] = ALPHABET
+    if !session["unplayed_letters"]
+        session["unplayed_letters"] = generate_alphabet
     end
     if params["choice"]
         choice_index = params["choice"].to_i
-        choice_letter = ALPHABET[choice_index]
-        @session["unplayed_letters"] = remove_choice(session["unplayed_letters"], choice_index)
-        flash_message = "Removed: " + choice_letter
+        session["unplayed_letters"] = remove_choice(session["unplayed_letters"], choice_index)
+        flash_message = "Removed: " + ALPHABET[choice_index]
     end
-    erb :index, :locals => {:word => @session["word"], :unplayed_letters => @session["unplayed_letters"], 
-        :alphabet => FULL_ALPHABET, :flash_message => flash_message}
+    word_letters = session["word"].split("")
+    erb :index, :locals => {:word => session["word"], :unplayed_letters => session["unplayed_letters"], 
+        :alphabet => ALPHABET, :flash_message => flash_message, :word_letters => word_letters}
 end
