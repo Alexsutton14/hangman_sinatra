e4d52abf0855a1b124baf2138ff94de057cbbcc0
Alex Sutton <alexsutton14@hotmail.com>
2019-10-25 14:50:50 +0100

    Added word choosing logic

--- hangman.rb
+++ hangman.rb
@@ -1,6 +1,14 @@
 require "sinatra"
 require "sinatra/reloader" if development?
 
+WORDS = [
+    "hangman"
+]
+
+def new_word()
+    return WORDS[rand(WORDS.length)]
+end
+
 get "/" do
-    "Hello World!"
+    new_word()
 end
